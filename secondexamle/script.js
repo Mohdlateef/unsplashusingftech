f9392cdebc5f6be2ecf473afc92e11721b1c6418
unknown <lateefahbaba123@gmail.com>
2024-01-27 00:43:49 +0530

    first

--- secondexamle/script.js
+++ secondexamle/script.js
@@ -9,20 +9,26 @@ getdata(inputtext);
 inputtext="";
 });
 let getdata=(text)=>{
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${text||'chicken'}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${text||"chicken"}`)
     .then((data)=>data.json())
     .then((data)=>{
-        console.log(data.meal);
-//  if(data.meal)
-//  {
-//     data.meal.Map((iteam)=>{
-//         let child1=document.createElement("div");
-//         chaild1.innerhtml=`<div>
+ console.log(data);
 
-//         </div>`
+ if(data.meals)
+ {
+    container.innerHTML=""
+    data.meals.map((iteam)=>{
+ let child=document.createElement("div");
 
-//     })
-//  }
+        child.innerHTML=`<div class="cards">
+        <img src="${iteam.strMealThumb}">
+        <p>${iteam.strMeal}</p>
+        </div>`
+        // console.log(iteam.strMealThumb)
+        container.appendChild(child);
+        // console.log(iteam.strMeal)
+    })
+}
     })
     .catch((error)=>console.log(error));
 
